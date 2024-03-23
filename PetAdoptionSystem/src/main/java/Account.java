88609cd3678c31b8db37ece441e4520254c11c50
MarcelloCV . <alithepistimonikos@gmail.com>
2024-03-23 10:47:58 +0000

    bro

--- PetAdoptionSystem/src/main/java/Account.java
+++ PetAdoptionSystem/src/main/java/Account.java
@@ -1,40 +1,44 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Account{
-  
-  private String username;
-  private String password; 
-  private static HashMap<String, String> accountMap = new HashMap<>();
 
+public class Account {
+
+    private String username;
+    private String password;
+    private static HashMap<String, String> accountMap = new HashMap<>();
     public Account(String username, String password) {
-    this.username = username;
-    this.password = password;
-    accountMap.put(username, password);
+        this.username = username;
+        this.password = password;
+        accountMap.put(username, password);
     }
 
-    // Getter for username
     public String getusername() {
         return username;
     }
+
     // Setter for username
     public void setusername(String username) {
         this.username = username;
     }
+
     // Getter for password
     public String getPassword() {
         return password;
     }
+
     // Setter for password
     public void setPassword(String password) {
         this.password = password;
     }
+
     //
     public static HashMap<String, String> getAccountMap() {
         return accountMap;
     }
+
     //
     public static void setAccountMap(String username, String password) {
         accountMap.put(username, password);
-    }  
+    }
 }
-
